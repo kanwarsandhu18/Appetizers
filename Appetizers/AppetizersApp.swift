51c644bc193c63321684711fb914b7a54e47a793
Kanwar <kanwarsandhu1809@gmail.com>
2024-08-03 22:01:01 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Kanwar Sandhu on 2024-08-03.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
